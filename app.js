486b10c5381ffc487bc86b7d2bf41351fbee7fc8
wenhuiwu6 <wenhuiwu678@gmail.com>
2025-10-08 12:32:40 -0400

    gan

---
+++ app.js
@@ -0,0 +1,97 @@
+const cats = [
+  {
+    name: "Maine Coon",
+    price: 2000,
+    inStock: true,
+    img: "https://pethelpful.com/.image/w_3840,q_auto:good,c_fill,ar_4:3/NDowMDAwMDAwMDAwMDYyOTgx/silver-maine-coon.jpg",
+    alt: "The Maine Coon is a large domesticated cat breed. One of the oldest natural breeds in North America, the breed originated in the U.S. state of Maine, where it is the official state cat. The Maine Coon is a large and social cat, commonly referred to as the gentle giant.",
+  },
+
+  {
+    name: "British Shorthair",
+    price: 1200, 
+    inStock: True,
+    img: "https://www.catster.com/wp-content/uploads/2023/11/grey-and-white-british-shorthair-cat_Nynke-van-Holten_Shutterstock-e1720621991666.jpg",
+    alt: "The British Shorthair is the pedigree version of the traditional British domestic cat, with a distinctively stocky body, thick coat, and broad face. The most familiar colour variant is the British Blue, with a solid grey-blue coat, copper-coloured eyes, and a medium-sized tail."
+  },
+
+  {
+    name: "Persian",
+    price: , 
+    inStock ,
+    img: "",
+    alt: ""
+  },
+  
+  {
+    name: "",
+    price: , 
+    inStock ,
+    img: "",
+    alt: ""
+  },
+  
+  {
+    name: "",
+    price: , 
+    inStock ,
+    img: "",
+    alt: ""
+  },
+
+  {
+    name: "",
+    price: , 
+    inStock ,
+    img: "",
+    alt: ""
+  },
+
+  {
+    name: "",
+    price: , 
+    inStock ,
+    img: "",
+    alt: ""
+  },
+
+  {
+    name: "",
+    price: , 
+    inStock ,
+    img: "",
+    alt: ""
+  },
+
+  {
+    name: "",
+    price: , 
+    inStock ,
+    img: "",
+    alt: ""
+  },
+
+  {
+    name: "",
+    price: , 
+    inStock ,
+    img: "",
+    alt: ""
+  },
+
+  {
+    name: "",
+    price: , 
+    inStock ,
+    img: "",
+    alt: ""
+  },
+  
+  {
+    name: "",
+    price: , 
+    inStock ,
+    img: "",
+    alt: ""
+  },
+];
