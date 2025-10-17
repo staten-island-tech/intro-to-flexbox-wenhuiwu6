1a2aa180907f0507e503de644208b3450153f62d
wenhuiwu6 <wenhuiwu678@gmail.com>
2025-10-17 12:32:32 -0400

    benzhu

--- Another/app.js
+++ Another/app.js
@@ -155,3 +155,17 @@ function inject(item) {
 }
 
 cats.forEach((item) => inject(item));
+
+// function addToCart() {
+//   const buttons = document.querySelectorAll("button");
+//   const btnArray = Array.from(buttons);
+//   btnArray.forEach((btn) =>
+//     btn.addEventListener("click", function (event) {
+//       console.log(event.target.textContent);
+//       console.log(
+//         event.target.closest(".display-card").getAttribute("data-title")
+//       );
+//     })
+//   );
+// }
+// addToCart();
