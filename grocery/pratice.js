e0f3b03673ce692b8b6f303daffce183fa625288
wenhuiwu6 <wenhuiwu678@gmail.com>
2025-11-13 12:32:11 -0500

    1

--- grocery/pratice.js
+++ grocery/pratice.js
@@ -1,6 +1,48 @@
-function takeNumber(num, list) {
-  while (num <= 999){
-    if ("")
-  }
-};
-takeNumber(23, ["TAKE", "TAKE", "SERVE", "TAKE", "SERVE", "SERVE", "CLOSE"]);
+function take(n, list) {
+  let late = 0;
+  let wait = 0;
+  let numbs = n;
+  list.forEach((slip) => {
+    if (slip === "TAKE") {
+      late++;
+      wait++;
+      numbs++;
+    } else if (slip === "SERVE") {
+      wait--;
+    } else if (slip === "CLOSE") {
+      console.log(late, wait, numbs);
+      wait = 0;
+      late = 0;
+    } else if ("EOF") {
+      return;
+    }
+  });
+}
+
+take(23, [
+  "TAKE",
+  "TAKE",
+  "SERVE",
+  "TAKE",
+  "SERVE",
+  "SERVE",
+  "CLOSE",
+  "TAKE",
+  "TAKE",
+  "TAKE",
+  "SERVE",
+  "CLOSE",
+  "TAKE",
+  "SERVE",
+  "TAKE",
+  "SERVE",
+  "TAKE",
+  "TAKE",
+  "TAKE",
+  "TAKE",
+  "TAKE",
+  "TAKE",
+  "SERVE",
+  "CLOSE",
+  "EOF",
+]);
